669bc7ffbd7d6bb23b9cd1fed746f4f7bc47c76e
Hieu Cao <hieu.cao@s3corp.com.vn>
2025-05-07 17:33:53 +0700

    product admin

---
+++ H_Shopping/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using H_Shopping.Services.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H_Shopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+        [Route("{area}/{controller}/")]
+        public IActionResult Index()
+        {
+            var listProduct = productService.GetAll();
+
+            return View(listProduct);
+        }
+    }
+}
